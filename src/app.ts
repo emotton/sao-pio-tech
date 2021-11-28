3915e8ae00d83f3dccfdf9133db968a341fef618
Eduardo Motton <emotton@gmail.com>
2021-11-28 19:11:07 -0300

    Inclusão do root

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,11 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
+
+app.get("/", (req, res)=>{
+  res.send("<h1>São Pio Tech</h1>");
+});
+
 app.use(express.json());
 app.use(routes);
 

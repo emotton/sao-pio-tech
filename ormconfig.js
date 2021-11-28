19a263104cb47ee997270eac3301f9baeb37e19d
Eduardo Motton <emotton@gmail.com>
2021-11-28 00:01:04 -0300

    Primeira subida

---
+++ ormconfig.js
@@ -0,0 +1,17 @@
+console.log(`DATABASE_URL : ${process.env.DATABASE_URL}`);
+module.exports = {
+  "url": process.env.DATABASE_URL,
+  "type": "postgres",
+  "entities": [
+    "dist/models/**/*.js"
+ ],
+ "migrations": [
+  "dist/database/migrations/**/*.js"
+],
+ "cli":{
+  "migrationsDir": [
+    "src/database/migrations/"
+  ],
+  "entitiesDir": "src/models"
+  }
+}
